b244f70ad1a957dacd3f661596e207ac782729e3
Tulani-Intellergy <tulani@intellergy.co.za>
2023-11-09 10:14:52 +0200

    Agent - rough ui for agent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import Login from './components/login/Login';
 import './App.css';
+import Agent from './components/agent/Agent';
+import Display from './components/display/Display';
+
 
 
 function App() {
   return (
     <main className="App">
+      <Agent/>
+      <Display/>
 
-      <Login/>
     </main> 
   );
 }
---
+++ src/components/agent/Agent.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import "./Agent.css";
+
+function Agent() {
+   
+  return (
+    <div className='container-agent'>
+        <div className=" section ">
+            <h2>On Que</h2>
+            <div className='card'>
+                <div className='card-descritpion'>
+                    <p># Ticket NO 2521616158151</p>
+                </div>
+                <div className="card-button">
+                    <button className='button-group'>Cancel</button>
+                    <button className='button-group'>Pause</button>
+                    <button className='button-group'>Done</button>
+                </div>
+            </div>
+            <div className='card'>
+                <div className='card-descritpion'>
+                    <p># Ticket NO 2521616158151</p>
+                </div>
+                <div className="card-button">
+                    <button className='button-group'>Cancel</button>
+                    <button className='button-group'>Pause</button>
+                    <button className='button-group'>Done</button>
+                </div>
+            </div>
+            <div className='card'>
+                <div className='card-descritpion'>
+                    <p># Ticket NO 2521616158151</p>
+                </div>
+                <div className="card-button">
+                    <button className='button-group'>Cancel</button>
+                    <button className='button-group'>Pause</button>
+                    <button className='button-group'>Done</button>
+                </div>
+            </div>
+            <div className='card'>
+                <div className='card-descritpion'>
+                    <p># Ticket NO 2521616158151</p>
+                </div>
+                <div className="card-button">
+                    <button className='button-group'>Cancel</button>
+                    <button className='button-group'>Pause</button>
+                    <button className='button-group'>Done</button>
+                </div>
+            </div>
+            
+        </div>
+        <div className="section ">
+            <h2>Serving</h2>
+            <div className='card'>
+                <div className='card-descritpion'>
+                    <p># Ticket NO 2521616158151</p>
+                </div>
+                <div className="card-button">
+                    <button className='button-group'>Cancel</button>
+                    <button className='button-group'>Pause</button>
+                    <button className='button-group'>Done</button>
+                </div>
+            </div>
+
+
+
+        </div>
+        <div className="section ">
+            <h2>Done</h2>
+            <div className='card'>
+                <div className='card-descritpion'>
+                    <p># Ticket NO 2521616158151</p>
+                </div>
+                <div className="card-button">
+                    <button className='button-group'>Cancel</button>
+                    <button className='button-group'>Pause</button>
+                    <button className='button-group'>Done</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Agent
