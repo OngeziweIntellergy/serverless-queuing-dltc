c2f99e12f8133f639c4a1a793026b11520f9f237
Tulani-Intellergy <tulani@intellergy.co.za>
2023-11-09 17:41:59 +0200

    add agent page

--- src/components/agent/Agent.jsx
+++ src/components/agent/Agent.jsx
@@ -1,11 +1,27 @@
 import React, { useState } from 'react';
 import Swal from 'sweetalert2';
-import "./Agent.css";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import "./Agent.css"; 
 import data from './../../data.json';
 
 function Agent() {
     const [tickets, setTickets] = useState(data);
+    const [searchQuery, setSearchQuery] = useState('');
 
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const handleTicketSelect = (ticket) => {
+        Swal.fire({
+            title: `Ticket NO ${ticket.ticketNumber}`,
+            html: `<strong>Status:</strong> ${ticket.status}<br/><strong>Description:</strong> ${ticket.description}`,
+            icon: 'info',
+            confirmButtonText: 'Close'
+        });
+    };
+
+    const filteredTickets = tickets.filter(ticket => ticket.ticketNumber.toString().includes(searchQuery));
 
 
 const handleAction = (id, action) => {
@@ -60,7 +76,7 @@ const handleAction = (id, action) => {
                     case 'Done':
                         return { ...ticket, status: 'Done' };
                     case 'Reinstate':
-                        return { ...ticket, status: 'On Que' };
+                        return { ...ticket, status: 'In Queue' };
                     default:
                         return ticket;
                 }
@@ -85,33 +101,60 @@ const handleAction = (id, action) => {
     };
 
     return (
-        <div className='container-agent'>
-            {['On Que', 'Serving', 'Done', 'Cancelled'].map((section) => (
-                <div className="section" key={section}>
-                    <h2>{section}</h2>
-                    {tickets.filter(ticket => ticket.status === section).map(ticket => (
-                        <div className='card' key={ticket.id}>
-                            <div className='card-description'>
-                                <p># Ticket NO {ticket.ticketNumber}</p>
-                            </div>
-                            <div className="card-button">
-                                {section === 'Cancelled' ? (
-                                    <button className='button-group' onClick={() => handleAction(ticket.id, 'Reinstate')}>Reinstate</button>
-                                ) : (
-                                    <button className='button-group' onClick={() => handleAction(ticket.id, 'Cancel')}>Cancel</button>
-                                )}
-                                {ticket.status === 'On Que' && (
-                                    <button className='button-group' onClick={() => handleAction(ticket.id, 'Serve')}>Serve</button>
-                                )}
-                                {section === 'Done' && (
-                                    <button className='button-group' onClick={() => handleReview(ticket.ticketNumber)}>Review</button>
-                                )}
-                            </div>
-                        </div>
+        <>
+        <div>
+            <input
+                type="text"
+                className="form-control mb-3"
+                placeholder="Search ticket by number..."
+                value={searchQuery}
+                onChange={handleSearchChange}
+            />
+            {searchQuery && (
+                <div className="list-group">
+                    {filteredTickets.map(ticket => (
+                        <button
+                            key={ticket.id}
+                            className="list-group-item list-group-item-action"
+                            onClick={() => handleTicketSelect(ticket)}
+                        >
+                            Ticket NO {ticket.ticketNumber}
+                        </button>
                     ))}
+                </div>
+            )}
+        </div>
+        <div className='container-agent  mt-4'>
+        
+            {['In Queue', 'Serving', 'Done', 'Cancelled'].map((section) => (
+                <div key={section} className="section mb-3">
+                    <h2>{section}</h2>
+                    <div className="d-flex flex-wrap">
+                        {tickets.filter(ticket => ticket.status === section).map(ticket => (
+                            <div className='card m-2' style={{ width: '18rem' }} key={ticket.id}>
+                                <div className='card-body'>
+                                    <h5 className='card-title'>Ticket NO {ticket.ticketNumber}</h5>
+                                    <div className="d-flex justify-content-between">
+                                        {section === 'Cancelled' ? (
+                                            <button className='btn btn-primary' onClick={() => handleAction(ticket.id, 'Reinstate')}>Reinstate</button>
+                                        ) : (
+                                            <button className='btn btn-danger' onClick={() => handleAction(ticket.id, 'Cancel')}>Cancel</button>
+                                        )}
+                                        {ticket.status === 'In Queue' && (
+                                            <button className='btn btn-success' onClick={() => handleAction(ticket.id, 'Serve')}>Serve</button>
+                                        )}
+                                        {section === 'Done' && (
+                                            <button className='btn btn-info' onClick={() => handleReview(ticket.ticketNumber)}>Review</button>
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             ))}
         </div>
+        </>
     );
 }
 
