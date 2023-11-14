9422b73f053ba23ad1a0a6681e38779fac8afe7c
ongeziwe <ongeziwe@intellergy.co.za>
2023-11-14 10:05:46 +0200

    adding files

--- api-project/terraform/api-gateway.tf
+++ api-project/terraform/api-gateway.tf
@@ -1,89 +1,135 @@
 ##############
 # API Gateway#
 ##############
-resource "aws_api_gateway_rest_api" "dltc_ticket_api" {
-  name        = "dltc_ticket_api"
-  description = "ticket API Gateway"
+
+resource "aws_api_gateway_rest_api" "transaction_apigw" {
+  name        = "transaction_apigw"
+  description = "Transaction API Gateway"
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 
-resource "aws_api_gateway_resource" "ticket" {
-  rest_api_id = aws_api_gateway_rest_api.dltc_ticket_api.id
-  parent_id   = aws_api_gateway_rest_api.dltc_ticket_api.root_resource_id
-  path_part   = "ticket"
+resource "aws_api_gateway_resource" "transaction" {
+  rest_api_id = aws_api_gateway_rest_api.transaction_apigw.id
+  parent_id   = aws_api_gateway_rest_api.transaction_apigw.root_resource_id
+  path_part   = "transaction"
 }
 
-resource "aws_api_gateway_resource" "list_tickets" {
-  rest_api_id = aws_api_gateway_rest_api.dltc_ticket_api.id
-  parent_id   = aws_api_gateway_rest_api.dltc_ticket_api.root_resource_id
-  path_part   = "list_tickets"
+resource "aws_api_gateway_resource" "list_transactions" {
+  rest_api_id = aws_api_gateway_rest_api.transaction_apigw.id
+  parent_id   = aws_api_gateway_rest_api.transaction_apigw.root_resource_id
+  path_part   = "list_transactions"
 }
 
-resource "aws_api_gateway_method" "storeticket" {
-  rest_api_id   = aws_api_gateway_rest_api.dltc_ticket_api.id
-  resource_id   = aws_api_gateway_resource.ticket.id
+resource "aws_api_gateway_method" "storetransaction" {
+  rest_api_id   = aws_api_gateway_rest_api.transaction_apigw.id
+  resource_id   = aws_api_gateway_resource.transaction.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "list_tickets" {
-  rest_api_id   = aws_api_gateway_rest_api.dltc_ticket_api.id
-  resource_id   = aws_api_gateway_resource.list_tickets.id
+resource "aws_api_gateway_method" "list_transactions" {
+  rest_api_id   = aws_api_gateway_rest_api.transaction_apigw.id
+  resource_id   = aws_api_gateway_resource.list_transactions.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "storeticket-lambda" {
-  rest_api_id = aws_api_gateway_rest_api.dltc_ticket_api.id
-  resource_id = aws_api_gateway_resource.ticket.id
-  http_method = aws_api_gateway_method.storeticket.http_method
+resource "aws_api_gateway_method" "deletetransaction" {
+  rest_api_id   = aws_api_gateway_rest_api.transaction_apigw.id
+  resource_id   = aws_api_gateway_resource.transaction.id
+  http_method   = "DELETE"
+  authorization = "NONE"
+}
 
+resource "aws_api_gateway_method" "updatetransaction" {
+  rest_api_id   = aws_api_gateway_rest_api.transaction_apigw.id
+  resource_id   = aws_api_gateway_resource.transaction.id
+  http_method   = "PUT" # or "PATCH" depending on your API design
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "storetransaction-lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.transaction_apigw.id
+  resource_id             = aws_api_gateway_resource.transaction.id
+  http_method             = aws_api_gateway_method.storetransaction.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-
-  uri = aws_lambda_function.StoreTicketHandler.invoke_arn
+  uri                     = aws_lambda_function.StoreTransactionHandler.invoke_arn
 }
 
-resource "aws_api_gateway_integration" "list_tickets-lambda" {
-  rest_api_id = aws_api_gateway_rest_api.dltc_ticket_api.id
-  resource_id = aws_api_gateway_resource.list_tickets.id
-  http_method = aws_api_gateway_method.list_tickets.http_method
-
+resource "aws_api_gateway_integration" "list_transactions-lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.transaction_apigw.id
+  resource_id             = aws_api_gateway_resource.list_transactions.id
+  http_method             = aws_api_gateway_method.list_transactions.http_method
   integration_http_method = "GET"
   type                    = "AWS_PROXY"
-
-  uri = aws_lambda_function.ListTicketsHandler.invoke_arn
+  uri                     = aws_lambda_function.ListTransactionsHandler.invoke_arn
 }
 
-resource "aws_lambda_permission" "apigw-StoreticketHandler" {
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.StoreTicketHandler.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.dltc_ticket_api.execution_arn}/*/POST/ticket"
+resource "aws_api_gateway_integration" "deletetransaction-lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.transaction_apigw.id
+  resource_id             = aws_api_gateway_resource.transaction.id
+  http_method             = aws_api_gateway_method.deletetransaction.http_method
+  integration_http_method = "POST" # Lambda uses POST for all invocations
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.DeleteTransactionHandler.invoke_arn
 }
 
-resource "aws_lambda_permission" "apigw-ListticketsHandler" {
+resource "aws_api_gateway_integration" "updatetransaction-lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.transaction_apigw.id
+  resource_id             = aws_api_gateway_resource.transaction.id
+  http_method             = aws_api_gateway_method.updatetransaction.http_method
+  integration_http_method = "POST" # Lambda uses POST for all invocations
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.UpdateTransactionHandler.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw-StoreTransactionHandler" {
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.ListTicketsHandler.function_name
+  function_name = aws_lambda_function.StoreTransactionHandler.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.dltc_ticket_api.execution_arn}/*/GET/list_tickets"
+  source_arn    = "${aws_api_gateway_rest_api.transaction_apigw.execution_arn}/*/POST/transaction"
+}
+
+resource "aws_lambda_permission" "apigw-ListTransactionsHandler" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.ListTransactionsHandler.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.transaction_apigw.execution_arn}/*/GET/list_transactions"
+}
+
+resource "aws_lambda_permission" "apigw-DeleteTransactionHandler" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.DeleteTransactionHandler.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.transaction_apigw.execution_arn}/*/DELETE/transaction"
+}
+
+resource "aws_lambda_permission" "apigw-UpdateTransactionHandler" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.UpdateTransactionHandler.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.transaction_apigw.execution_arn}/*/PUT/transaction" # or PATCH
 }
 
 resource "aws_cloudwatch_log_group" "main_api_gw" {
-  name              = "/aws/api-gw/${aws_api_gateway_rest_api.dltc_ticket_api.name}"
+  name              = "/aws/api-gw/${aws_api_gateway_rest_api.transaction_apigw.name}"
   retention_in_days = 14
 }
 
-
-resource "aws_api_gateway_deployment" "ticket_apigw_deployment" {
+resource "aws_api_gateway_deployment" "transaction_apigw_deployment" {
   depends_on = [
-    aws_api_gateway_method.storeticket,
-    aws_api_gateway_method.list_tickets,
-    aws_api_gateway_integration.storeticket-lambda,
-    aws_api_gateway_integration.list_tickets-lambda,
+    aws_api_gateway_method.storetransaction,
+    aws_api_gateway_method.list_transactions,
+    aws_api_gateway_method.deletetransaction,
+    aws_api_gateway_method.updatetransaction,
+    aws_api_gateway_integration.storetransaction-lambda,
+    aws_api_gateway_integration.list_transactions-lambda,
+    aws_api_gateway_integration.deletetransaction-lambda,
+    aws_api_gateway_integration.updatetransaction-lambda,
   ]
-  rest_api_id = aws_api_gateway_rest_api.dltc_ticket_api.id
+  rest_api_id = aws_api_gateway_rest_api.transaction_apigw.id
   stage_name  = "Dev"
 }
--- api-project/terraform/lambda.tf
+++ api-project/terraform/lambda.tf
@@ -1,5 +1,6 @@
-resource "aws_iam_role" "TicketLambdaRole" {
-  name               = "TicketLambdaRole"
+
+resource "aws_iam_role" "TransactionLambdaRole" {
+  name               = "TransactionLambdaRole"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -17,72 +18,126 @@ resource "aws_iam_role" "TicketLambdaRole" {
 EOF
 }
 
-data "template_file" "ticketlambdapolicy" {
+data "template_file" "transactionlambdapolicy" {
   template = file("${path.module}/policy.json")
 }
 
-resource "aws_iam_policy" "TicketLambdaPolicy" {
-  name        = "TicketLambdaPolicy"
+resource "aws_iam_policy" "TransactionLambdaPolicy" {
+  name        = "TransactionLambdaPolicy"
   path        = "/"
-  description = "IAM policy for Ticket lambda functions"
-  policy      = data.template_file.ticketlambdapolicy.rendered
+  description = "IAM policy for Transaction lambda functions"
+  policy      = data.template_file.transactionlambdapolicy.rendered
 }
 
-resource "aws_iam_role_policy_attachment" "TicketLambdaRolePolicy" {
-  role       = aws_iam_role.TicketLambdaRole.name
-  policy_arn = aws_iam_policy.TicketLambdaPolicy.arn
+resource "aws_iam_role_policy_attachment" "TransactionLambdaRolePolicy" {
+  role       = aws_iam_role.TransactionLambdaRole.name
+  policy_arn = aws_iam_policy.TransactionLambdaPolicy.arn
 }
 
 data "archive_file" "lambda_zip_store" {
   type        = "zip"
   source_dir  = "../lambda"
-  output_path = "../lambda/StoreTicketHandler.zip"
+  output_path = "../lambda/StoreTransactionHandler.zip"
 }
 
 data "archive_file" "lambda_zip_list" {
   type        = "zip"
   source_dir  = "../lambda"
-  output_path = "../lambda/ListTicketsHandler.zip"
+  output_path = "../lambda/ListTransactionsHandler.zip"
 }
 
-resource "aws_lambda_function" "StoreTicketHandler" {
-  function_name = "StoreTicketHandler"
-  filename      = data.archive_file.lambda_zip_store.output_path
-  handler       = "StoreTicketHandler.lambda_handler"
-  runtime       = "python3.8"
+data "archive_file" "lambda_zip_delete" {
+  type        = "zip"
+  source_dir  = "../lambda"
+  output_path = "../lambda/DeleteTransactionHandler.zip"
+}
 
+data "archive_file" "lambda_zip_update" {
+  type        = "zip"
+  source_dir  = "../lambda"
+  output_path = "../lambda/UpdateTransactionHandler.zip"
+}
+
+resource "aws_lambda_function" "StoreTransactionHandler" {
+  function_name = "StoreTransactionHandler"
+  filename      = data.archive_file.lambda_zip_store.output_path
+  handler       = "StoreTransactionHandler.lambda_handler"
+  runtime       = "python3.8"
   environment {
     variables = {
       REGION            = "us-east-1"
-      ticket_table = aws_dynamodb_table.ticket_table.name
+      TRANSACTION_TABLE = aws_dynamodb_table.transaction_table.name
     }
   }
-
-  role        = aws_iam_role.TicketLambdaRole.arn
+  role        = aws_iam_role.TransactionLambdaRole.arn
   timeout     = "5"
   memory_size = "128"
 }
 
-resource "aws_lambda_function" "ListTicketsHandler" {
-  function_name = "ListTicketsHandler"
+resource "aws_lambda_function" "ListTransactionsHandler" {
+  function_name = "ListTransactionsHandler"
   filename      = data.archive_file.lambda_zip_list.output_path
-  handler       = "ListTicketsHandler.lambda_handler"
+  handler       = "ListTransactionsHandler.lambda_handler"
   runtime       = "python3.8"
-
   environment {
     variables = {
       REGION            = "us-east-1"
-      ticket_table = aws_dynamodb_table.ticket_table.name
+      TRANSACTION_TABLE = aws_dynamodb_table.transaction_table.name
     }
   }
+  role        = aws_iam_role.TransactionLambdaRole.arn
+  timeout     = "5"
+  memory_size = "128"
+}
 
-  role        = aws_iam_role.TicketLambdaRole.arn
+resource "aws_lambda_function" "DeleteTransactionHandler" {
+  function_name = "DeleteTransactionHandler"
+  filename      = data.archive_file.lambda_zip_delete.output_path
+  handler       = "DeleteTransactionHandler.lambda_handler"
+  runtime       = "python3.8"
+  environment {
+    variables = {
+      REGION            = "us-east-1"
+      TRANSACTION_TABLE = aws_dynamodb_table.transaction_table.name
+    }
+  }
+  role        = aws_iam_role.TransactionLambdaRole.arn
+  timeout     = "5"
+  memory_size = "128"
+}
+
+resource "aws_lambda_function" "UpdateTransactionHandler" {
+  function_name = "UpdateTransactionHandler"
+  filename      = data.archive_file.lambda_zip_update.output_path
+  handler       = "UpdateTransactionHandler.lambda_handler"
+  runtime       = "python3.8"
+  environment {
+    variables = {
+      REGION            = "us-east-1"
+      TRANSACTION_TABLE = aws_dynamodb_table.transaction_table.name
+    }
+  }
+  role        = aws_iam_role.TransactionLambdaRole.arn
   timeout     = "5"
   memory_size = "128"
 }
 
 resource "aws_cloudwatch_log_group" "lambda_log_group_store" {
-  name = "/aws/lambda/${aws_lambda_function.StoreTicketHandler.function_name}"
+  name              = "/aws/lambda/${aws_lambda_function.StoreTransactionHandler.function_name}"
+  retention_in_days = 14
+}
 
+resource "aws_cloudwatch_log_group" "lambda_log_group_list" {
+  name              = "/aws/lambda/${aws_lambda_function.ListTransactionsHandler.function_name}"
+  retention_in_days = 14
+}
+
+resource "aws_cloudwatch_log_group" "lambda_log_group_delete" {
+  name              = "/aws/lambda/${aws_lambda_function.DeleteTransactionHandler.function_name}"
+  retention_in_days = 14
+}
+
+resource "aws_cloudwatch_log_group" "lambda_log_group_update" {
+  name              = "/aws/lambda/${aws_lambda_function.UpdateTransactionHandler.function_name}"
   retention_in_days = 14
 }
