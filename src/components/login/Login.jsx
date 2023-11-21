5e02659b3a965b84b43b0889186999e2028f2f1f
hlengiwet <hlengiwet@intellergy.co.za>
2023-11-21 14:10:24 +0200

    updating login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,63 +3,49 @@ import './Login.css';
 
 function Login() {
   const [cellphone, setCellphone] = useState('');
-  //const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [activities, setActivities] = useState({
+    ProfessionalDrivingPermit: false,
+    OperatingLicence: false,
+    MotorVehicleLicense: false,
+    DriverRenewalLicense: false,
+  });
+  const [selectedStations, setSelectedStations] = useState([]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    try {
-        const response = await fetch('http://your-backhttps://pzv5egxa4k.execute-api.us-east-1.amazonaws.com/Prodend-api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ cellphone, password }),
-        });
+    // You'll need to modify this to send the activities and selectedStations
+    // as part of your request payload.
 
-        const data = await response.json();
-        
-        if (response.ok) {
-            // Handle successful login
-            // e.g., saving the token, updating the authentication state
-        } else {
-            // Handle errors, e.g., show a message to the user
-        }
-    } catch (error) {
-        console.error('Registration failed:', error);
-        // Handle network error
-    }
-};
+    // ...
+  };
 
+  const handleActivityChange = (event) => {
+    setActivities({
+      ...activities,
+      [event.target.value]: event.target.checked,
+    });
+  };
 
+  const handleStationSelect = (event) => {
+    setSelectedStations([...event.target.selectedOptions].map(o => o.value));
+  };
 
   return (
     <div className="login-container">
-      {/* <img src="https://dltccoffeeimages.s3.amazonaws.com/Gauteng+transport.jpeg"></img> */}
       <div className="login-box">
-        <h2 className="login-title">Register here</h2>
+        <h2 className="login-title">Login</h2>
         <form className="login-form" onSubmit={handleSubmit}>
-          {/* <div className="input-group">
-            <label htmlFor="email">Email:</label>
+          <div className="input-group">
+            <label htmlFor="cellphone">User Email:</label>
             <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              type="tel"  
+              id="cellphone" 
+              value={cellphone} 
+              onChange={(e) => setCellphone(e.target.value)} 
               required
             />
-          </div> */}
-
-          <div className="input-group">
-          <label htmlFor="cellphone">Cellphone Number:</label>
-          <input
-             type="tel"  
-             id="cellphone" 
-             value={cellphone} 
-             onChange={(e) => setCellphone(e.target.value)} 
-             required
-           />
           </div>
 
           <div className="input-group">
@@ -73,16 +59,34 @@ function Login() {
             />
           </div>
 
-          <div className="input-group">
-          <label htmlFor="dropdown">What will you be doing Today? </label>
-          <select id="options" name="options" >
-          <option value="Professional Driving Permit">Professional Driving Permit</option>
-          <option value="Operating Licence">Operating Licence</option>
-          <option value="Motor Vehicle License">Motor Vehicle License</option>
-          <option value="Driver Renewal Licenc">Driver Renewal Licenc</option>
-          
-        </select>
-          </div>
+          <fieldset className="input-group">
+            <legend>What will you be doing Today?</legend>
+            {Object.keys(activities).map((activity) => (
+              <label key={activity}>
+                <input
+                  type="checkbox"
+                  value={activity}
+                  checked={activities[activity]}
+                  onChange={handleActivityChange}
+                />
+                {activity}
+              </label>
+            ))}
+          </fieldset>
+
+              <div className="input-group">
+              <label htmlFor="stations">Select Station:</label>
+              <select
+                id="stations"
+                name="stations"
+                value={selectedStations}
+                onChange={handleStationSelect}
+              >
+                {Array.from({ length: 16 }, (_, i) => (
+                  <option key={i} value={`Station ${i + 1}`}>{`Station ${i + 1}`}</option>
+                ))}
+              </select>
+            </div>
 
           <button type="submit" className="login-button">Login</button>
         </form>
@@ -92,4 +96,3 @@ function Login() {
 }
 
 export default Login;
-
