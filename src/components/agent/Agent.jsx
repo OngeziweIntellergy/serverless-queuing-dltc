c39b18020c76b368b1aa46576faf840c951f4761
hlengiwet <hlengiwet@intellergy.co.za>
2023-11-22 23:07:10 +0200

    adding tts on agent

--- src/components/agent/Agent.jsx
+++ src/components/agent/Agent.jsx
@@ -7,10 +7,21 @@ import "./Agent.css";
 function Agent() {
     const [tickets, setTickets] = useState([]);
     const [searchQuery, setSearchQuery] = useState('');
+
+    // Text-to-Speech Function
+    const speakText = (text) => {
+        if ('speechSynthesis' in window) {
+          const utterance = new SpeechSynthesisUtterance(text);
+          window.speechSynthesis.speak(utterance);
+        } else {
+          console.error("Your browser does not support text-to-speech.");
+        }
+    };
+
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.post('https://bbkzcze7c3.execute-api.us-east-1.amazonaws.com/Dev/list_tickets'); // Replace with your data URL
+                const response = await axios.post('https://bbkzcze7c3.execute-api.us-east-1.amazonaws.com/Dev/list_tickets');
                 let data = response.data;
                 data = data.sort((a, b) => new Date(a.datetime) - new Date(b.datetime));
 
@@ -42,118 +53,24 @@ function Agent() {
         });
     };
 
-    // const filteredTickets = tickets.filter(ticket => ticket.ticketNumber.toString().includes(searchQuery));
-    const filteredTickets = tickets.filter(ticket => ticket.ticketNumber && ticket.ticketNumber.toString().includes(searchQuery));
-
-
-    const handleSignOut = () => {
-        //This is the sign out link redirecting you back to sign in
-        window.location.href = 'https://frontend.d17g06z7kjqaor.amplifyapp.com/login';
-    };
-
-
-    // const handleAction = (id, action) => {
-    //     let updatedTickets = [...tickets];
-    //     let currentlyServing = updatedTickets.find(ticket => ticket.state === 'Serving');
-
-    //     if (action === 'Serve') {
-    //         if (currentlyServing) {
-    //             // If there is a ticket currently being served, move it to 'Done'
-    //             updatedTickets = updatedTickets.map(ticket => {
-    //                 if (ticket.ticket_id === currentlyServing.ticket_id) {
-    //                     return { ...ticket, state: 'Done' };
-    //                 }
-    //                 return ticket;
-    //             });
-    //         }
-    //         // Set the selected ticket to 'Serving'
-    //         updatedTickets = updatedTickets.map(ticket => {
-    //             if (ticket.ticket_id === id) {
-    //                 return { ...ticket, state: 'Serving' };
-    //             }
-    //             return ticket;
-    //         });
-    //     } else {
-    //         // Handle other actions (Cancel, Done, Reinstate)
-    //         updatedTickets = updatedTickets.map(ticket => {
-    //             if (ticket.ticket_id === id) {
-    //                 let newState = '';
-    //                 switch (action) {
-    //                     case 'Cancel':
-    //                         newState = 'Cancel';
-    //                         break;
-    //                     case 'Done':
-    //                         newState = 'Done';
-    //                         break;
-    //                     case 'Reinstate':
-    //                         newState = 'in Queue';
-    //                         break;
-    //                     default:
-    //                         newState = ticket.state; // No change in state
-    //                 }
-    //                 return { ...ticket, state: newState };
-    //             }
-    //             return ticket;
-    //         });
-    //     }
-
-    //     setTickets(updatedTickets);
-    // };
-
     const handleAction = async (id, action) => {
         let updatedTickets = [...tickets];
 
-        if (action === 'Cancel') {
-            const ticketToCancel = updatedTickets.find(ticket => ticket.ticket_id === id);
-            if (ticketToCancel && ticketToCancel.state === 'Serving') {
-                const result = await Swal.fire({
-                    title: 'Cancel Ticket',
-                    text: 'Select a reason for not attending the customer:',
-                    input: 'select',
-                    inputOptions: {
-                        'no-show': 'Customer No-Show',
-                        'closed': 'Service Closed',
-                        'other': 'Other'
-                    },
-                    inputPlaceholder: 'Select a reason',
-                    showCancelButton: true
-                });
-
-                if (!result.isConfirmed) {
-                    return; // Exit the function if cancelled
-                }
-                // If confirmed, proceed to update the ticket state to 'Cancel'
-            }
-        }
-
-        if (action === 'Reinstate') {
-            // Remove the ticket from its current position and add it to the start of the queue
-            updatedTickets = updatedTickets.filter(ticket => ticket.ticket_id !== id);
-            updatedTickets.unshift({ ...tickets.find(ticket => ticket.ticket_id === id), state: 'in Queue' });
-        } else if (action === 'Serve') {
-            // Move any currently serving ticket to 'Done'
-            updatedTickets = updatedTickets.map(ticket => 
-                ticket.state === 'Serving' ? { ...ticket, state: 'Done' } : ticket
-            );
-            // Set the selected ticket to 'Serving'
+        if (action === 'Serve') {
             updatedTickets = updatedTickets.map(ticket => 
                 ticket.ticket_id === id ? { ...ticket, state: 'Serving' } : ticket
             );
-        } else {
-            // Update the ticket state for other actions (Cancel, Done)
-            updatedTickets = updatedTickets.map(ticket => {
-                if (ticket.ticket_id === id) {
-                    return { ...ticket, state: action };
-                }
-                return ticket;
-            });
+
+            const ticketBeingServed = updatedTickets.find(ticket => ticket.ticket_id === id);
+            if (ticketBeingServed) {
+                const ticketNumber = ticketBeingServed.ticketNumber;
+                const stationNumber = "1"; // Replace with actual station number if available
+                speakText(`Now serving ticket number ${ticketNumber} at station number ${stationNumber}.`);
+            }
         }
 
         setTickets(updatedTickets);
     };
-
-
-
 
     const handleReview = (ticketNumber) => {
         Swal.fire({
@@ -164,67 +81,72 @@ function Agent() {
         });
     };
 
+    const handleSignOut = () => {
+        window.location.href = 'https://frontend.d17g06z7kjqaor.amplifyapp.com/login';
+    };
+
+    const filteredTickets = tickets.filter(ticket => ticket.ticketNumber && ticket.ticketNumber.toString().includes(searchQuery));
+
     return (
         <>
+            <div className="sign-out-container">
+                <button className="btn btn-secondary" onClick={handleSignOut}>
+                    Sign Out
+                </button>
+            </div>
 
-        <div className="sign-out-container">
-            <button className="btn btn-secondary" onClick={handleSignOut}>
-                Sign Out
-            </button>
-        </div>
-
-        <div>
-            <input
-                type="text"
-                className="form-control mb-3"
-                placeholder="Search ticket by number..."
-                value={searchQuery}
-                onChange={handleSearchChange}
-            />
-            {searchQuery && (
-                <div className="list-group">
-                    {filteredTickets.map(ticket => (
-                        <button
-                            key={ticket.ticket_id}
-                            className="list-group-item list-group-item-action"
-                            onClick={() => handleTicketSelect(ticket)}
-                        >
-                            Ticket NO {ticket.ticketNumber}
-                        </button>
-                    ))}
-                </div>
-            )}
-        </div>
-        <div className='container-agent  mt-4'>
-        
-            {['in Queue', 'Serving', 'Done', 'Cancel'].map((section) => (
-                <div key={section} className="section mb-3">
-                    <h2>{section}</h2>
-                    <div className="d-flex flex-wrap">
-                        {tickets.filter(ticket => ticket.state === section).map(ticket => (
-                            <div className='card m-2' style={{ width: '18rem' }} key={ticket.ticket_id}>
-                                <div className='card-body'>
-                                    <h5 className='card-title'>Ticket NO {ticket.ticketNumber}</h5>
-                                    <div className="d-flex justify-content-between">
-                                        {section === 'Cancel' ? (
-                                            <button className='btn btn-primary' onClick={() => handleAction(ticket.ticket_id, 'Reinstate')}>Reinstate</button>
-                                        ) : (
-                                            <button className='btn btn-danger' onClick={() => handleAction(ticket.ticket_id, 'Cancel')}>Cancel</button>
-                                        )}
-                                        {ticket.state === 'in Queue' && (
-                                            <button className='btn btn-success' onClick={() => handleAction(ticket.ticket_id, 'Serve')}>Serve</button>
-                                        )}
-                                        {section === 'Done' && (
-                                            <button className='btn btn-info' onClick={() => handleReview(ticket.ticketNumber)}>Review</button>
-                                        )}
-                                    </div>
-                                </div>
-                            </div>
+            <div>
+                <input
+                    type="text"
+                    className="form-control mb-3"
+                    placeholder="Search ticket by number..."
+                    value={searchQuery}
+                    onChange={handleSearchChange}
+                />
+                {searchQuery && (
+                    <div className="list-group">
+                        {filteredTickets.map(ticket => (
+                            <button
+                                key={ticket.ticket_id}
+                                className="list-group-item list-group-item-action"
+                                onClick={() => handleTicketSelect(ticket)}
+                            >
+                                Ticket NO {ticket.ticketNumber}
+                            </button>
                         ))}
                     </div>
-                </div>
-            ))}
-        </div>
+                )}
+            </div>
+
+            <div className='container-agent mt-4'>
+                {['in Queue', 'Serving', 'Done', 'Cancel'].map((section) => (
+                    <div key={section} className="section mb-3">
+                        <h2>{section}</h2>
+                        <div className="d-flex flex-wrap">
+                            {tickets.filter(ticket => ticket.state === section).map(ticket => (
+                                <div className='card m-2' style={{ width: '18rem' }} key={ticket.ticket_id}>
+                                    <div className='card-body'>
+                                        <h5 className='card-title'>Ticket NO {ticket.ticketNumber}</h5>
+                                        <div className="d-flex justify-content-between">
+                                            {section === 'Cancel' ? (
+                                                <button className='btn btn-primary' onClick={() => handleAction(ticket.ticket_id, 'Reinstate')}>Reinstate</button>
+                                            ) : (
+                                                <button className='btn btn-danger' onClick={() => handleAction(ticket.ticket_id, 'Cancel')}>Cancel</button>
+                                            )}
+                                            {ticket.state === 'in Queue' && (
+                                                <button className='btn btn-success' onClick={() => handleAction(ticket.ticket_id, 'Serve')}>Serve</button>
+                                            )}
+                                            {section === 'Done' && (
+                                                <button className='btn btn-info' onClick={() => handleReview(ticket.ticketNumber)}>Review</button>
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
