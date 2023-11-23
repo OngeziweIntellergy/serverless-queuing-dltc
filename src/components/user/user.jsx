4b07bcf2b5cae9153c77fcc05fda1de6aa8def5a
ByroneIntel <byrone@intellergy.co.za>
2023-11-23 04:53:54 +0200

    user- dashboard

--- src/components/user/user.jsx
+++ src/components/user/user.jsx
@@ -1,53 +1,132 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import axios from 'axios';
+import './User.css'
+import Swal from 'sweetalert2';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { field: 'id', headerName: 'Number', width: 250 },
+  { field: 'username', headerName: 'Username', width: 250 },
+  { field: 'email', headerName: 'Email', width: 250 },
+  { field: 'User_Role', headerName: 'User Role', width: 250 },
+  { field: 'station', headerName: 'Station', width: 250 },
+  // ... other columns
 ];
 
 export default function User() {
-  return (
-    <div style={{ height: 400, width: '80%'}}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-      />
-    </div>
-  );
+    const [rows, setRows] = useState([]);
+
+    useEffect(() => {
+        const fetchOptions = async () => {
+          try {
+            const result = await axios.post('https://op0unjbx79.execute-api.us-east-1.amazonaws.com/Dev/List');
+            const respData = result.data.statusCode;
+            const resp = result.data.body;
+            
+            if (respData === 200) {
+                const jsonObject = JSON.parse(resp);
+                const transformedRows = jsonObject.map((item, index) => ({
+                    id: index + 1, // Assuming you don't have a unique 'id' field in your data
+                    ...item
+                }));
+                setRows(transformedRows);
+            } else {
+                // Handle non-successful response
+            }
+          } catch (error) {
+            console.error(error);
+          }
+        };
+        fetchOptions();
+    }, []);
+
+    const handleAddUser = () => {
+        Swal.fire({
+            title: 'Add New User',
+            html:
+                '<input id="username" class="swal2-input" placeholder="Username">' +
+                '<input id="email" class="swal2-input" placeholder="Email">' +
+                '<input id="password" class="swal2-input" placeholder="Password">' +
+                '<input id="User Role" class="swal2-input" placeholder="User Role">' +
+                '<input id="station" class="swal2-input" placeholder="Station">',
+            confirmButtonText: 'Add User',
+            focusConfirm: false,
+            preConfirm: () => {
+                const user = {
+                    // username: "hjsdfiv",
+                    // email:"hjgsuidfhi",
+                    // user_Role: "kijvd",
+                    // station:"4"
+                    username: document.getElementById('username').value,
+                    email: document.getElementById('email').value,
+                    password: document.getElementById('password').value,
+                    User_Role: document.getElementById('User Role').value,
+                    station: document.getElementById('station').value
+                };
+                // const response =  await fetch('https://op0unjbx79.execute-api.us-east-1.amazonaws.com/Dev/register', {
+                //     method: 'POST',
+                //     headers: {
+                //         'Content-Type': 'application/json',                    
+                //     },
+                //     body: JSON.stringify(user),
+                // });
+    
+                // Replace with your API endpoint
+                const apiEndpoint = 'https://op0unjbx79.execute-api.us-east-1.amazonaws.com/Dev/register';
+    
+                // // Make POST request to add the user
+                axios.post(apiEndpoint,user).then(response => {
+                        // console.log(JSON.parse(response))
+                        // console.log(JSON.parse(response.data))
+                        console.log(response)
+
+                        // Handle the response here. Example:
+                        if(response.status){
+                            Swal.fire(response.data);
+                            window.location.reload(false);
+                            
+                        } else {
+                            Swal.fire('Error', 'Could not add user', 'error');
+                        }
+                    })
+                    .catch(error => {
+                        // Handle errors here
+                        Swal.fire('Error', 'Request failed: ' + error.message, 'error');
+                    });
+            }
+        });
+    };
+    
+
+    return (
+        <div className="data-grid-container">
+            <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '10px', marginRight: '30px'  }}>
+                <button onClick={handleAddUser} className="add-user-button">Add User</button>
+            </div>
+            <DataGrid
+                className="data-grid"
+                rows={rows}
+                columns={columns}
+                pageSize={5}
+                checkboxSelection
+                pagination
+                />
+                </div>
+            );
+
+    {/* // return (
+    //     <div style={{ height: 400, width: '100%' }}>
+            
+    //         <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '10px', marginRight: '30px' }}>
+    //         <button onClick={handleAddUser} style={{ padding: '10px', fontSize: '16px' }}>Add User</button>
+    //         </div>
+    //         <DataGrid
+    //             rows={rows}
+    //             columns={columns}
+    //             pageSize={5}
+    //             checkboxSelection
+    //             pagination
+    //         />
+    //     </div>
+    // ); */}
 }
