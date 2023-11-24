d36440bb002adf8f49adf9999697ac111cc2103f
Tulani-Intellergy <tulani@intellergy.co.za>
2023-11-24 04:02:09 +0200

    Ticket- EST addition but not working

--- src/components/ticket/Ticket.jsx
+++ src/components/ticket/Ticket.jsx
@@ -7,6 +7,7 @@ const Ticket = () => {
   const [reason, setReason] = useState('');
   const [ticketState, setTicketState] = useState('');
   const [numberInQueue, setNumberInQueue] = useState('');
+  const [estimatedServiceTime, setEstimatedServiceTime] = useState('');
 
   useEffect(() => {
     const storedTicketData = localStorage.getItem('requestDetails');
@@ -22,7 +23,9 @@ const Ticket = () => {
 
   const countTicketsBefore = (tickets, currentTicketDateTime, option) => {
     return tickets.filter(ticket => 
-      new Date(ticket.datetime) < new Date(currentTicketDateTime) && ticket.option === option
+      new Date(ticket.datetime) < new Date(currentTicketDateTime) &&
+      ticket.option === option &&
+      ticket.state === 'in Queue'
     ).length;
   };
 
@@ -32,9 +35,22 @@ const Ticket = () => {
       const tickets = response.data;
       const numberBefore = countTicketsBefore(tickets, dateTime, option);
       setNumberInQueue(numberBefore);
+      calculateEstimatedServiceTime(numberBefore);
     } catch (error) {
       console.error("Error fetching tickets: ", error);
     }
+  };
+
+  const calculateEstimatedServiceTime = (numberBefore) => {
+    const averageWaitTimePerPerson = 15; // Average wait time in minutes per person
+    const totalWaitTime = numberBefore * averageWaitTimePerPerson; // Total wait time in minutes
+
+    const currentTime = new Date();
+    currentTime.setMinutes(currentTime.getMinutes() + totalWaitTime);
+
+    // Converting to South African Standard Time (SAST) UTC+2
+    const sastTime = new Date(currentTime.getTime() + (2 * 60 * 60 * 1000)); // Adding 2 hours for SAST
+    setEstimatedServiceTime(sastTime.toLocaleTimeString('en-ZA', { hour: '2-digit', minute: '2-digit' }));
   };
 
   return (
@@ -47,6 +63,7 @@ const Ticket = () => {
         <div className="ticket-number-container">
           <p className="ticket-number">Ticket NO: <span className='ticket-number-color'>{ticketNumber}</span> </p>
           <p>Number in Queue: {numberInQueue}</p>
+          <p>Estimated Service Time: {estimatedServiceTime}</p>
           <small>Reason for visit: {reason}</small>
         </div>
       </div>
