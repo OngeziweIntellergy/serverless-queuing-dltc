cf36d3cbac0d45cfde71deb95c7ced0fc7d951e9
Tulani-Intellergy <tulani@intellergy.co.za>
2023-12-11 11:51:47 +0200

    login css

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,131 +1,136 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Login.css'; // Assuming you have a CSS file for styling
+import { setUserSession } from '../../service/AuthService';
+import axios from 'axios';
+import './Login.css'; 
+import {useNavigate} from 'react-router-dom';
+import Swal from 'sweetalert2';
+
+
 
 function Login() {
+  const navigate = useNavigate();
+  const [password, setPassword]= useState('');
+  const [username, setUsername]= useState('');
+  const [reason, setReason] = useState({
+    Professional_Driving_Permit: false,
+    Diver_Renewal_License: false,
+    Motor_Vehicle_License: false,
+    Operating_License: false,
+    checkAll: false,
+  })
+  const updateCheckboxes = (checked) => {
+    document.getElementById('Professional_Driving_Permit').checked = checked;
+    document.getElementById('Diver_Renewal_License').checked = checked;
+    document.getElementById('Motor_Vehicle_License').checked = checked;
+    document.getElementById('Operating_License').checked = checked;
+  };
+  const submitHandler =(e)=>{
+    e.preventDefault();
+    let loginUrl = "https://hps0z6b4xb.execute-api.us-east-1.amazonaws.com/prod/login";
+    const requestConfig = {
+      headers: {
+      'x-api-key': 'gNzOqOhI5771BmV6rw2962Usc4rpTtnPvxlWzVf4'
+      }
+    }
+    const requestBody ={
+      username:username,
+      password:password 
+    }
+    const updateBody ={
+      station: reason
+    }
+    axios.post(loginUrl, requestBody, requestConfig).then(response=>{
+      setUserSession(response.data.user, response.data.token)
+      showOptions();
+      console.log(response)}).catch(error=>{
+        console.log(error)
+        // if(error.response.status){
+        //   console.log(error.response.data.message)
+        // }else{
+        //   console.log("server error.")
+        // }
 
-  const navigate = useNavigate(); // Create an instance of useNavigate for navigation
-  const [username, setUsername] = useState(''); // State for username
-  const [password, setPassword] = useState(''); // State for password
-  const [activities, setActivities] = useState({ // State for activities
-    ProfessionalDrivingPermit: false,
-    OperatingLicence: false,
-    MotorVehicleLicense: false,
-    DriverRenewalLicense: false,
-  });
-  const [selectedStations, setSelectedStations] = useState([]); // State for selected stations
-  const [errorMessage, setErrorMessage] = useState(''); // State for error messages
+    })
+  }
 
-  // Function to handle checkbox changes
-  const handleActivityChange = (event) => {
-    setActivities({
-      ...activities,
-      [event.target.value]: event.target.checked,
+
+  const showOptions = () => {
+    Swal.fire({
+      title: 'Select Options',
+      html: `
+        <div style="text-align: left;">
+          <label>
+            <input type="checkbox" id="Professional_Driving_Permit">
+            Professional Driving Permit
+          </label><br>
+
+          <label>
+            <input type="checkbox" id="Diver_Renewal_License">
+            Diver Renewal License
+          </label><br>
+
+          <label>
+            <input type="checkbox" id="Motor_Vehicle_License">
+            Motor Vehicle License
+          </label><br>
+
+          <label>
+            <input type="checkbox" id="Operating_License">
+            Operating License
+          </label><br>
+
+          <label>
+            <input type="checkbox" id="checkAll" class="check-all">
+            Check All
+          </label>
+        </div>
+      `,
+      didOpen: () => {
+        document.getElementById('checkAll').addEventListener('change', (e) => {
+          updateCheckboxes(e.target.checked);
+        });
+      },
+      preConfirm: () => {
+        return {
+          Professional_Driving_Permit: document.getElementById('Professional_Driving_Permit').checked,
+          Diver_Renewal_License: document.getElementById('Diver_Renewal_License').checked,
+          Motor_Vehicle_License: document.getElementById('Motor_Vehicle_License').checked,
+          Operating_License: document.getElementById('Operating_License').checked,
+          checkAll: document.getElementById('checkAll').checked,
+        };
+      },
+      showCancelButton: true,
+      confirmButtonText: 'Submit',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        setReason(result.value);
+        console.log('Selected options:', result.value);
+        navigate('/user')
+        // Send data to your endpoint
+        // Example: axios.post('your-endpoint', result.value)
+      }
     });
   };
 
-  // Function to handle station selection
-  const handleStationSelect = (event) => {
-    setSelectedStations([...event.target.selectedOptions].map(o => o.value));
-  };
-
-  // Payload for login request
-  const payload = {
-    username,
-    password,
-    activities,
-    selectedStations
-  };
-
-  // Function to handle form submission
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await fetch('https://op0unjbx79.execute-api.us-east-1.amazonaws.com/Dev/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',                    
-        },
-        body: JSON.stringify(payload),
-      });
-
-      if (response.ok) {
-        navigate('/agent');
-      } else {
-        const responseData = await response.json();
-        setErrorMessage(responseData.message || 'Login failed');
-      }
-    } catch (error) {
-      console.error('Error submitting form:', error);
-      setErrorMessage('An error occurred during login.');
-    }
-  };
-
   return (
-    <div className="login-container">
-      <div className="login-box">
-        <img src="https://dltccoffeeimages.s3.amazonaws.com/new_logo_dltc.png" alt="Smart Licensing Logo" className="login-logo"/>
-        <h2 className="login-title">Login</h2>
-        <p className="welcome-text">Welcome to SMART LICENSING</p>
 
-        {errorMessage && <p className="error-message">{errorMessage}</p>} {/* Display error message */}
-
-        <form className="login-form" onSubmit={handleSubmit}>
-          <div className="input-group">
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </div>
-
-          <div className="input-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-
-          <fieldset className="input-group">
-            <legend>What will you be doing Today?</legend>
-            {Object.keys(activities).map((activity) => (
-              <label key={activity}>
-                <input
-                  type="checkbox"
-                  value={activity}
-                  checked={activities[activity]}
-                  onChange={handleActivityChange}
-                />
-                {activity}
-              </label>
-            ))}
-          </fieldset>
-
-          <div className="input-group">
-            <label htmlFor="stations">Select Station:</label>
-            <select
-              id="stations"
-              name="stations"
-              value={selectedStations}
-              onChange={handleStationSelect}
-            >
-              {Array.from({ length: 16 }, (_, i) => (
-                <option key={i} value={`Station ${i + 1}`}>{`Station ${i + 1}`}</option>
-              ))}
-            </select>
-          </div>
-
-          <button type="submit" className="login-button">Login</button>
-        </form>
-      </div>
+    <div className="registration-form">
+      <form onSubmit={submitHandler}>
+        <div className="form-icon">
+            <span><i className="icon icon-user"></i></span>
+        </div>
+        <div className="form-group">
+            <input type="text" className="form-control item"  value={username} onChange={(e) => setUsername(e.target.value)} id="username" placeholder="Username" required/>
+        </div>
+        <div className="form-group">
+            <input type="password" className="form-control item" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
+        </div>
+        <div className="form-group">
+            <input type="submit" className="btn btn-block create-account" value="Login" />
+        </div>
+      </form>
     </div>
+
   );
 }
 
