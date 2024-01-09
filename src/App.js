c77d2913da3990c2502c4105b17056614dbf6d14
hlengiwet <hlengiwet@intellergy.co.za>
2024-01-09 10:59:58 +0200

    adding page not found

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Dashboard from './pages/dashboard/Dashboard';
 import Agent from './pages/agent/Agent';
-import Display from './pages/display/Display';
 import Reason from './pages/reason/Reason';
 import Ticket from './pages/ticket/Ticket';
 import Login from './pages/login/Login';
 import User from './pages/user/user';
 import Register from './pages/register/Register';
 import WaitingArea from './pages/waitingarea/WaitingArea';
-
-
+import NotFound from './pages/notfound/NotFound';
 
 function App() {
   return (
@@ -19,24 +17,21 @@ function App() {
       <div className="App">
         
         <Routes>
-          <Route path="/" element={<Display />} />
+          <Route path="/" element={<WaitingArea />} />
+          <Route path="/WaitingArea" element={<WaitingArea />} />
           <Route path="/reason" element={<Reason />} />
           <Route path="/login" element={<Login />} />
           <Route path="/agent" element={<Agent />} />
           <Route path="/ticket" element={<Ticket />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/waitingarea" element={<WaitingArea />} />
           <Route path="/dashboard" element={<Dashboard />}/>
           <Route path="/user" element={<User />} />
-          <Route path="*" element={<Display />} /> 
+          <Route path="*" element={<NotFound />} /> 
          
-          
         </Routes>
       </div>
     </Router>
-
         
-
   );
 }
 
